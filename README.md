44a8e5e6901ac7df00e7e42fa52855fc6dcda9f7
JILSE7 <saidnnnn@gmail.com>
2022-05-13 17:28:37 -0500

    proyecto terminado

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# Webpack configurado
+# Practica Unidad 3
 
-Para usar este repositorio, deberán ejecutar el comando de ```npm install```
+Materia: Tecnologías de seguridad en software 
 
-Pero esto lo veremos en la siguiente sección
+![Instituto Tecnologia de Tlalnepantla](https://www.exevi.com/wp-content/uploads/2020/09/auditorias-de-ciberseguridad-tipos-escenarios-de-aplicacion-y-resultados-esperados.jpg)
+* **Said Mandujano Garcia**.<br>
+* **16251121**.<br>
+* **Mtde Hilda Diaz Rincón**.<br>
+
+
+
--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,8 +1,50 @@
+(() => {
+    const modal = document.getElementById("myModal");
+    const btn = document.getElementById("btnModal");
+    const btnAcept = document.querySelector(".btn-accept");
+    const span = document.getElementsByClassName("close")[0];
+    const body = document.getElementsByTagName("body")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+        body.style.position = "static";
+        body.style.height = "100%";
+        body.style.overflow = "hidden";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+
+            body.style.position = "inherit";
+            body.style.height = "auto";
+            body.style.overflow = "visible";
+        }
+    }
+
+    btnAcept.onclick = function() {
+        modal.style.display = "none";
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
+    }
+
+
+    btn.click();
+})()
 const logo = document.querySelector("#logo");
 const nav = document.querySelector("#nav");
-
-
 
 gsap.from(logo, {duration: 3, x: 3200, delay: 0.5, borderRadius:"50%"});
 gsap.from(nav, {duration: 3, x: -3200, delay: 0.5});
 gsap.to(".danger", {duration: 1, x: 0, scale: 2, y: 10}, "greyAndPink") 
+
+
